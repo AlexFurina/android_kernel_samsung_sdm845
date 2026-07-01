8e902050a138a56b959db4fa84e8c352e7ab7479
AlexFurina <92114905+AlexFurina@users.noreply.github.com>
2026-07-02 01:20:42 +0900

    fs: fuse: reset

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -155,10 +155,6 @@ struct fuse_file {
 
 	/** Has flock been performed on this file? */
 	bool flock:1;
-
-	/* the read write file */
-	struct file *passthrough_filp;
-	bool passthrough_enabled;
 };
 
 /** One input argument of a request */
@@ -238,7 +234,6 @@ struct fuse_args {
 		unsigned argvar:1;
 		unsigned numargs;
 		struct fuse_arg args[2];
-		struct file *passthrough_filp;
 	} out;
 };
 
@@ -391,9 +386,6 @@ struct fuse_req {
 
 	/** Request is stolen from fuse_file->reserved_req */
 	struct file *stolen_file;
-
-	/** fuse passthrough file  */
-	struct file *passthrough_filp;
 };
 
 struct fuse_iqueue {
@@ -553,9 +545,6 @@ struct fuse_conn {
 
 	/** handle fs handles killing suid/sgid/cap on write/chown/trunc */
 	unsigned handle_killpriv:1;
-
-	/** passthrough IO. */
-	unsigned passthrough:1;
 
 	/*
 	 * The following bitfields are only for optimization purposes
